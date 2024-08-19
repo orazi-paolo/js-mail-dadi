ccf7def06c4f71f5c86add16818c6d8572472180
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-19 13:28:57 +0200

    create random numbers

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Esercizio 1
+/*
+1. genera un numero random da 1 a 6 sia per il giocatore sia per il computer
+2. stabilisci il vincitore in base al punteggio piu alto
+3. stampa in pagina i due tiri e il risultato
+*/
+// creo il primo numero causale
+let casualNumberOne = Math.floor(Math.random() * 6) + 1;
+console.log(casualNumberOne);
+
+// creo il secondo numero casuale
+let casualNumberTwo = Math.floor(Math.random() * 6) +1;
+console.log(casualNumberTwo);
